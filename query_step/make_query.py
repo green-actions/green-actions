9db44eeada2b475b4e4035362fac1d28d4d8071e
Patrick J. Roddy <patrickjamesroddy@gmail.com>
2026-04-30 14:05:30 +0100

    FOrmat

--- query_step/make_query.py
+++ query_step/make_query.py
@@ -7,6 +7,7 @@ def get_github_token(secrets_file):
     github_token = ""
     import yaml
     from pathlib import Path
+
     with Path(secrets_file).open() as f:
         file_contents = yaml.safe_load(f)
         if "github_token" in file_contents.keys():
@@ -23,12 +24,12 @@ def get_auth_headers():
 def get_list_of_workflows(org: str, repo: str):
     ### Gets a list of workflows from the given repo
 
-
     url = f"https://api.github.com/repos/{org}/{repo}/actions/runs"
 
     kwargs = get_auth_headers()
 
     return requests.get(url, **kwargs)
+
 
 def get_one_workflow_info(org: str, repo: str, workflow_run: str):
     url = f"https://api.github.com/repos/{org}/{repo}/actions/runs/{workflow_run}"
@@ -36,26 +37,29 @@ def get_one_workflow_info(org: str, repo: str, workflow_run: str):
     kwargs = get_auth_headers()
     return requests.get(url, **kwargs)
 
+
 def get_workflow_runtime_info(org: str, repo: str, workflow_run: str):
     ### Extract the timing information from a single workflow
 
-    url = f"https://api.github.com/repos/{org}/{repo}/actions/runs/{workflow_run}/timing"
+    url = (
+        f"https://api.github.com/repos/{org}/{repo}/actions/runs/{workflow_run}/timing"
+    )
 
     kwargs = get_auth_headers()
 
     return requests.get(url, **kwargs)
 
+
 def make_csv_output(data):
     ### Output the data to CSV format
 
-    with open("output.csv","w",newline='') as f:
-        w = csv.DictWriter(f,list(data[0].keys()))
+    with open("output.csv", "w", newline="") as f:
+        w = csv.DictWriter(f, list(data[0].keys()))
         w.writeheader()
         w.writerows(data)
 
 
 if __name__ == "__main__":
-
     auth_headers = get_auth_headers()
 
     org_name = "green-actions"
@@ -74,7 +78,9 @@ if __name__ == "__main__":
         tmp_output["branch"] = wf_run["head_branch"]
         tmp_output["start_time"] = wf_run["run_started_at"]
 
-        timing_info = get_workflow_runtime_info(org_name,repo_name,wf_run["id"]).json()
+        timing_info = get_workflow_runtime_info(
+            org_name, repo_name, wf_run["id"]
+        ).json()
 
         if "run_duration_ms" in timing_info.keys():
             tmp_output["duration"] = timing_info["run_duration_ms"]
@@ -83,5 +89,3 @@ if __name__ == "__main__":
             print("No timing")
 
     make_csv_output(output_dict)
-
-
